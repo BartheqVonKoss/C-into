c931cb8f63c607d4dc53d25ac4545544990acbe0
BartheqVonKoss <37189954+BartheqVonKoss@users.noreply.github.com>
2018-04-17 14:19:12 +0200

    Create sol_4_2.hpp

---
+++ L4/sol_4_2.hpp
@@ -0,0 +1,17 @@
+//
+//  sol_4_2.hpp
+//  L4
+//
+//  Created by Bartłomiej Kos on 15/04/2018.
+//  Copyright © 2018 Bartłomiej Kos. All rights reserved.
+//
+
+#ifndef sol_4_2_hpp
+#define sol_4_2_hpp
+
+#include <stdio.h>
+
+int fibo1(int n);
+int fibo2(int n);
+
+#endif /* sol_4_2_hpp */
