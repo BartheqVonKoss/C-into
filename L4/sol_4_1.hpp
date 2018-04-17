ddb36857d823a85485f3e4690ef19cfa0af9ac13
BartheqVonKoss <37189954+BartheqVonKoss@users.noreply.github.com>
2018-04-17 14:19:50 +0200

    Create sol_4_1.hpp

---
+++ L4/sol_4_1.hpp
@@ -0,0 +1,17 @@
+//
+//  sol_4_1.hpp
+//  L4
+//
+//  Created by Bartłomiej Kos on 15/04/2018.
+//  Copyright © 2018 Bartłomiej Kos. All rights reserved.
+//
+
+#ifndef sol_4_1_hpp
+#define sol_4_1_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+double &operator*(std::vector<double>& v, std::vector<double>& w);
+#endif /* sol_4_1_hpp */
