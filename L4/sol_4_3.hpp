93bf4e8dd8921d8f0ad97cf130b9e4ea78b3d6cf
BartheqVonKoss <37189954+BartheqVonKoss@users.noreply.github.com>
2018-06-04 17:46:02 +0200

    Create sol_4_3.hpp

---
+++ L4/sol_4_3.hpp
@@ -0,0 +1,17 @@
+//
+//  sol_4_3.hpp
+//  L4Z3
+//
+//  Created by Bartłomiej Kos on 23/05/2018.
+//  Copyright © 2018 Bartłomiej Kos. All rights reserved.
+//
+
+#ifndef sol_4_3_hpp
+#define sol_4_3_hpp
+
+#include <stdio.h>
+
+double sumuj(double (*f)(int), int m, double &eps);
+
+#endif /* sol_4_3_hpp */
+
